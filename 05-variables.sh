8064672923bf4cc20eabdacdd659a201cc6c32d9
Srinivasa Phanindra V <sphanindra@gmail.com>
2026-07-16 15:32:58 +0530

    actions

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,7 +5,7 @@ read USER_NAME
 
 echo "User name is $USER_NAME"
 
-# echo "Please enter your password::"
-# read -s PASSWORD
+echo "Please enter your password::"
+read -s PASSWORD
 
-# echo "Password is $PASSWORD"
+echo "Password is $PASSWORD"
