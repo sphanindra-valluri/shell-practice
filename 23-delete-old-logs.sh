daaea66dfb99c63da7e2cfca958fb4dc559c04b4
Srinivasa Phanindra V <sphanindra@gmail.com>
2026-07-30 00:14:44 +0530

    actions

--- 23-delete-old-logs.sh
+++ 23-delete-old-logs.sh
@@ -14,15 +14,15 @@ if [ ! -d $LOGS_DIR ]; then
 fi
 
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
-echo "$FILES_TO_DELETE"
+# echo "$FILES_TO_DELETE"
 
-# if [ -z "$FILES_TO_DELETE" ]; then
-#     echo "No files older than 14 days to delete."
-#   else
-#     while IFS= read -r filepath; do
-#       # Process each line here
-#       echo "Deleting file: $filepath"
-#       rm -f $filepath
-#       echo "Deleted file: $filepath"
-#     done <<< $FILES_TO_DELETE
-# fi
+if [ -z "$FILES_TO_DELETE" ]; then
+    echo "No files older than 14 days to delete."
+  else
+    while IFS= read -r filepath; do
+      # Process each line here
+      echo "Deleting file: $filepath"
+      rm -f $filepath
+      echo "Deleted file: $filepath"
+    done <<< $FILES_TO_DELETE
+fi
